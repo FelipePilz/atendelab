cd4404e7c58c62bf8dbe7acc67fd6203474114f0
Felipe Pilz <felipe.pilz@univille.br>
2026-06-08 20:46:48 -0300

    Criação inicial do banco de dados

--- database/atendelab.sql
+++ database/atendelab.sql
@@ -13,6 +13,40 @@ CREATE TABLE `usuarios` (
   `criado_em` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+CREATE TABLE `pessoas` (
+  `id` int(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `nome` varchar(100) NOT NULL,
+  `documento` varchar(20) NOT NULL UNIQUE,
+  `telefone` varchar(20) NOT NULL,
+  `curso` varchar(100) NOT NULL,
+  `periodo` varchar(100) NOT NULL,
+  `status` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+CREATE TABLE `tipos_atendimentos` (
+  `id` int(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `nome` varchar(100) NOT NULL,
+  `descricao` TEXT NOT NULL,
+  `status` ENUM('ativo', 'inativo') DEFAULT 'ativo'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+CREATE TABLE `atendimentos` (
+  `id` int(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `pessoa_id` int(11), 
+  `tipo_atendimento_id` int(11) NOT NULL,
+  `usuario_id` int(11),
+  `data_atendimento` DATE NOT NULL DEFAULT (CURRENT_DATE),
+  `hora_atendimento` TIME NOT NULL DEFAULT (CURRENT_TIME),
+  `descricao` TEXT NOT NULL,
+  `observacao` TEXT,
+  `status` ENUM('ativo', 'inativo') DEFAULT 'ativo',
+  `criado_em` timestamp NOT NULL DEFAULT current_timestamp(),
+  CONSTRAINT `fk_atendimentos_pessoas` FOREIGN KEY (`pessoa_id`) REFERENCES `pessoas` (`id`) ON DELETE SET NULL,
+  CONSTRAINT `fk_atendimentos_tipos` FOREIGN KEY (`tipo_atendimento_id`) REFERENCES `tipos_atendimentos` (`id`),
+  CONSTRAINT `fk_atendimentos_usuarios` FOREIGN KEY (`usuario_id`) REFERENCES `usuarios` (`id`) ON DELETE SET NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
 INSERT INTO usuarios (nome, email, senha, perfil, status)
 VALUES (
  'Administrador',
